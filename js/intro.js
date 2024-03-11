0f270ddecad58b55cb7038e57b330ee366f7d33f
kavuki <teahkcabral@gmail.com>
2024-03-11 15:25:28 -0700

    update

--- js/intro.js
+++ js/intro.js
@@ -1,2 +1,43 @@
-//resource link: https://www.w3schools.com/jquery/default.asp
+$(document).ready(function() {
 
+  $("#popup-box").hide();
+    $('.thirtythree').hover(function() {
+      var $this = $(this);
+      var target = $this.data('target');
+      $({ countNum: $this.text() }).animate({
+        countNum: target + '%'
+      }, {
+        duration: 5000,
+        easing: 'linear',
+        step: function() {
+          $this.text(Math.floor(parseFloat(this.countNum)) + '%');
+        },
+        complete: function() {
+          $this.text(target + '%');
+        }
+      });
+    }, function() {
+      var $this = $(this);
+      var target = $this.data('target');
+      $this.text('0%');
+    });
+
+      $('.latte').mouseenter(function() {
+        $(this).attr('src', 'media/images/latte broke.png'); 
+      }).mouseleave(function() {
+        $(this).attr('src', 'media/images/latte full.png'); 
+      });
+
+      $('.guam').mouseenter(function() {
+        $(this).attr('src', 'media/images/guam broke.png'); 
+      }).mouseleave(function() {
+        $(this).attr('src', 'media/images/guam full.png');
+      });
+
+      $("#pic-box").hover(function(){
+        $("#popup-box").slideToggle(2000);
+    });
+
+  });
+
+  
